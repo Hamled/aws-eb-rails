75c0857afcd00f3713f86c3278fdd68c0fd079d9
Charles Ellis <hamled@hamled.name>
2016-03-08 08:35:14 -0800

    Elastic Load Balancer health ping route

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,8 @@ class WelcomeController < ApplicationController
   def index
     render plain: "This is a Rails website."
   end
+
+  def health
+    render nothing: true
+  end
 end
--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -5,4 +5,9 @@ class WelcomeControllerTest < ActionController::TestCase
     get :index
     assert_response :success
   end
+
+  test "should get health" do
+    get :health
+    assert_response :success
+  end
 end
